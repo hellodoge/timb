fef20bb166d677f2a88411bc70e654d3554ab222
waldemar <hellodoge@tuta.io>
2021-06-03 21:51:44 +0300

    (Post service) remove constructor from interface

--- service/PostServiceInterface.php
+++ service/PostServiceInterface.php
@@ -2,10 +2,7 @@
 
 namespace service;
 
-use repository\models\PostRepositoryInterface;
-
 interface PostServiceInterface
 {
-    public function __construct(PostRepositoryInterface $repo);
     public function getRecent(int $limit, int $offset);
 }
