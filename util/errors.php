e79593ef93100131b1a57855c90fca35b7e44dd2
waldemar <hellodoge@tuta.io>
2021-06-03 23:52:51 +0300

    printStackTrace will print error message as well

--- util/errors.php
+++ util/errors.php
@@ -7,5 +7,6 @@ use Exception;
 function printStackTrace(Exception $e)
 {
     $stderr = fopen('php://stderr', 'w');
+    fwrite($stderr, $e->getMessage() . PHP_EOL);
     fwrite($stderr, $e->getTraceAsString() . PHP_EOL);
 }
