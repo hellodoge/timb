40ec4c11049bb85e35795efb7c01080f22d082c7
waldemar <hellodoge@tuta.io>
2021-06-03 21:32:10 +0300

    add middleware.php (getting id from authorization header)

--- app/errors.php
+++ app/errors.php
@@ -5,6 +5,7 @@ namespace app;
 const INTERNAL_SERVER_ERROR = 500;
 const NOT_FOUND = 404;
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 const OK = 200;
 
 function sendResponse(int $status, string $message) {
---
+++ app/middleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app;
+
+use service\exceptions\ServiceException;
+use service\UserServiceInterface;
+
+const BEARER_TOKEN_PREFIX = "Bearer ";
+
+function GetUserId(UserServiceInterface $service): ?int
+{
+    if (!isset($_SERVER['HTTP_AUTHORIZATION']))
+    {
+        sendResponse(UNAUTHORIZED, "Empty authorization header");
+        return null;
+    }
+    $header = $_SERVER['HTTP_AUTHORIZATION'];
+    if (!str_starts_with($header, BEARER_TOKEN_PREFIX))
+    {
+        sendResponse(UNAUTHORIZED, "Expected Bearer authorization header");
+        return null;
+    }
+    $token = substr($header, strlen(BEARER_TOKEN_PREFIX));
+
+    try
+    {
+        return $service->parseToken($token);
+    }
+    catch (ServiceException $e)
+    {
+        sendResponse(UNAUTHORIZED, $e->getMessage());
+        return null;
+    }
+}
